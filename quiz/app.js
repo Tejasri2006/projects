47856603aade4f79245fb15d5b4f48747cb25a86
Tejasri2006 <srinivasbadeganti@gmail.com>
2025-06-06 23:34:50 +0530

    adding quiz mini project

---
+++ quiz/app.js
@@ -0,0 +1,102 @@
+const questions = [
+  {
+    question: "What is the capital city of Japan?",
+    options: ["Seoul", "Beijing", "Tokyo", "Bangkok"],
+    correctInd: 2,
+  },
+  {
+    question: "Who is known as the 'Father of the Nation' in India?",
+    options: [
+      "Jawaharlal Nehru",
+      "Subhas Chandra Bose",
+      "Mahatma Gandhi",
+      "Sardar Vallabhbhai Patel",
+    ],
+    correctInd: 2,
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Earth", "Venus", "Jupiter", "Mars"],
+    correctInd: 3,
+  },
+  {
+    question: "Who wrote the national anthem of India, 'Jana Gana Mana'?",
+    options: [
+      "Rabindranath Tagore",
+      "Bankim Chandra Chatterjee",
+      "Sarojini Naidu",
+      "Aurobindo Ghosh",
+    ],
+    correctInd: 0,
+  },
+  {
+    question: "What is the largest continent in the world by area?",
+    options: ["Africa", "North America", "Asia", "Europe"],
+    correctInd: 2,
+  },
+];
+
+let qpara = document.getElementById("q");
+let apara = document.querySelectorAll(".options");
+let next = document.querySelector("#next");
+let container = document.querySelector("#container");
+let i = 0;
+
+let currentQ;
+let score;
+let answered;
+function startQuiz() {
+  i = 0;
+  score = 0;
+  answered = [false, false, false, false, false];
+}
+startQuiz();
+display(i);
+function display(i) {
+  qpara.innerText = questions[i].question;
+  for (let j = 0; j < 4; j++) {
+    apara[j].innerText = `${j + 1}.${questions[i].options[j]}`;
+    apara[j].style.backgroundColor = "";
+  }
+}
+
+for (let k = 0; k < 4; k++) {
+  apara[k].addEventListener("click", function () {
+    if (answered[i] == true) {
+      for (let l = 0; l < 4; l++) {
+        apara[l].style.cursor = "not-allowed";
+      }
+    }
+    if (k === questions[i].correctInd) {
+      apara[k].style.backgroundColor = "rgba(0,255,0,0.5)";
+      score += 1;
+    } else {
+      apara[questions[i].correctInd].style.backgroundColor =
+        "rgba(0,255,0,0.5)";
+      apara[k].style.backgroundColor = "rgb(255, 0, 0, 0.5)";
+    }
+    answered[i] = true;
+  });
+}
+
+next.addEventListener("click", function (event) {
+  i += 1;
+  if (i == 5) {
+    event.preventDefault();
+    let scoreCard = document.createElement("div");
+    let character = document.createElement("div");
+    scoreCard.classList.add("score");
+    character.classList.add("character");
+    character.style.borderRadius = "10px";
+    document.body.append(scoreCard);
+    let message = document.createElement("div");
+    message.innerHTML = `<h2>Congratulations!!</h2><p>Your final score is: ${score}</p>`;
+    scoreCard.appendChild(character);
+    scoreCard.appendChild(message);
+    container.remove();
+
+    return;
+  } else {
+    display(i);
+  }
+});
