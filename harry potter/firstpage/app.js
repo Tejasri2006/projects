22bf9227202acdd6c69ceaa92d015a2c6ed92de1
Tejasri Badiganti <140103266+Tejasri2006@users.noreply.github.com>
2025-06-17 19:19:40 +0530

    Update app.js

--- harry potter/firstpage/app.js
+++ harry potter/firstpage/app.js
@@ -10,15 +10,15 @@ list.addEventListener("click", () => {
 });
 
 movies.addEventListener("click", () => {
-  window.location.href = "projects/harry%20potter/secondpage/index.html";
+  window.location.href = "/projects/harry%20potter/secondpage/index.html";
 });
 
 home.addEventListener("click", () => {
-  window.location.href = "projects/harry%20potter/secondpage/index.html";
+  window.location.href = "/projects/harry%20potter/secondpage/index.html";
 });
 
 search.addEventListener("click", () => {
   if (input.value.toLowerCase() === "movies") {
-    window.location.href = "projects/harry%20potter/secondpage/index.html";
+    window.location.href = "/projects/harry%20potter/secondpage/index.html";
   }
 });
